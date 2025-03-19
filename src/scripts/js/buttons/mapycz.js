a38b7b77ff021da59e993080219fd8960f8260f9
František Nesveda <frantisek.nesveda@gmail.com>
2025-03-19 15:03:26 +0100

    Fix Mapy.cz behavior

--- src/scripts/js/buttons/mapycz.js
+++ src/scripts/js/buttons/mapycz.js
@@ -1,29 +1,48 @@
 // adds map switcher buttons to Mapy.cz
 async function addButtonsToMapyCZ() {
 	var options = await loadOptionsFromStorage();
-	// find the structure containing the buttons
-	var mcButtonGroup = document.querySelector(".map-control .button-group");
+
+	const currentMapInfo = getMapInfo();
+
+	// remove the old button wrapper if present
+	var buttonWrapper = document.getElementById("map-switch-button-wrapper");
+	if (buttonWrapper != null) {
+		buttonWrapper.remove();
+	}
+
+	// create a new element structure containing the buttons to other map providers
+	buttonWrapper = document.createElement("mapy-map-button-group");
+	buttonWrapper.setAttribute("id", "map-switch-button-wrapper");
 	
-	// remove the old buttons if they're present
-	mcButtonGroup.querySelectorAll(".map-switch-button").forEach(elem => elem.remove());
-	
+
 	// add Google Maps button if desired
 	if (options.showGMapsButton) {
-		var GMSwitchButton = document.createElement("button");
+		var GMSwitchButton = document.createElement("a");
 		GMSwitchButton.setAttribute("class", "map-switch-button map-switch-button-google-maps");
-		GMSwitchButton.addEventListener("click", function(e) {switchMapTo("googlemaps", e.currentTarget);});
+		GMSwitchButton.setAttribute("href", getDestinationUrl("googlemaps", currentMapInfo));
+		GMSwitchButton.setAttribute("target", "_blank");
+		GMSwitchButton.setAttribute("rel", "noopener");
+		GMSwitchButton.addEventListener("mouseenter", updateButtonHrefs)
+
 		var GMSwitchButtonSpan = document.createElement("span");
+		GMSwitchButtonSpan.setAttribute("class", "map-switch-button-text");
 		GMSwitchButtonSpan.innerText = chrome.i18n.getMessage("google_maps");
 		GMSwitchButton.appendChild(GMSwitchButtonSpan);
-		mcButtonGroup.insertBefore(GMSwitchButton, mcButtonGroup.firstChild);
+
+		buttonWrapper.appendChild(GMSwitchButton);
+	}
+
+	if (buttonWrapper.hasChildNodes()) {
+		// find where to insert the button wrapper in the webpage
+		var topLeftTools = document.querySelector(".map-controls__topToolbar__leftTools");
+		while (topLeftTools == null) {
+			topLeftTools = document.querySelector(".map-controls__topToolbar__leftTools");
+			await new Promise(resolve => setTimeout(resolve, 100));
+		}
+		
+		// insert it there
+		topLeftTools.appendChild(buttonWrapper);
 	}
 }
 
-// normally, we shouldn't need to wait for window.onload, as content scripts should be injected on document_idle by default
-// but something changed in Firefox 67+ and the content scripts are sometimes executed earlier
-if (document.readyState === 'complete') { // window.onload has already fired
-	addButtonsToMapyCZ();
-}
-else {
-	window.addEventListener('load', addButtonsToMapyCZ);
-}
+runOnPageLoad(addButtonsToMapyCZ);
